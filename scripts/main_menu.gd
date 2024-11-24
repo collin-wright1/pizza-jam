225abc2b236eea2f4668412e83841a09bf469e11
Ethorax <51216497+Ethorax@users.noreply.github.com>
2024-11-23 21:12:49 -0500

    Main Menu functionality

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -10,3 +10,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	animation_player.play("menu",-1,0.2)
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game_controller.tscn")
+
+
+func _on_instruction_button_pressed() -> void:
+	$"Instruction Panel".show()
+
+
+func _on_instruction_exit_pressed() -> void:
+	$"Instruction Panel".hide()
